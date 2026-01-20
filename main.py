120d4a15c0533ab41fb2fd9d846167cbf958cfbb
jickzx <kingkongjay1@gmail.com>
2026-01-20 23:04:23 +0000

    dependencies

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+from io import BytesIO
+import base64
