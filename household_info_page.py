8fd6215a5fbb21c6dc50cb5ca57bec74ba0e76c9
PixelRG <thomasnirmal21@gmail.com>
2026-01-31 14:16:30 +0000

    Adding a file that will have functions to display the properties of every household

---
+++ household_info_page.py
@@ -0,0 +1,13 @@
+"""
+
+Todo: Make a sustainability function:
+Input:  This will need the sustainabilty score from the file
+
+Output: Should display a sustainability score
+
+
+Todo: Make a price display graph function:
+Input: 
+
+Todo: Make a household description
+"""
